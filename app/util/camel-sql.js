e361bd8d2715aa4682e7d1de9746cc9f184edb4f
Tim Davis <contact@timdav.is>
2016-11-27 14:23:04 -0800

    Adds test coverage reporting.

--- app/util/camel-sql.js
+++ app/util/camel-sql.js
@@ -1,14 +1,14 @@
 import { formatKeys } from 'deep-clone'
-import { camelCase, snakeCase, isFunction } from 'lodash'
+import { camelCase, snakeCase } from 'lodash'
 
 const snakeKeys = formatKeys(snakeCase)
 const camelKeys = formatKeys(camelCase)
+
 const wrap = fn => async (...args) => {
   return camelKeys(await fn(...args.map(snakeKeys)))
 }
 
 export default function camelSql(obj) {
-  if (isFunction(obj)) return wrap(obj)
   return Object.keys(obj).reduce((wrapped, key) => ({
     ...wrapped,
     [key]: wrap(obj[key])

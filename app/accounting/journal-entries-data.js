5af3c03439fefffd753093362b8421ccf4f2361e
thebearingedge <contact@timdav.is>
2016-12-24 17:22:14 -0800

    Moves journal_entries_view into data access code.

--- app/accounting/journal-entries-data.js
+++ app/accounting/journal-entries-data.js
@@ -19,10 +19,35 @@ export default function journalEntriesData(knex) {
   }
 
   function findById(id, trx) {
-    return trx
-      .select('*')
-      .from('journal_entries_view')
+    return journalEntriesView(trx)
       .where({ id })
       .first()
   }
+}
+
+function journalEntriesView(knex) {
+  const transaction = [
+    't.id',
+    't.date',
+    't.memo',
+    't.created_at',
+    't.updated_at'
+  ]
+  const ledger_entries = knex.raw(`
+    coalesce(
+      json_agg(
+        json_build_object(
+          'debit_code', le.debit_code,
+          'credit_code', le.credit_code,
+          'amount', le.amount
+        )
+      ) filter (where le is not null),
+      '[]'::json
+    ) as ledger_entries
+  `)
+  return knex
+    .select([ ...transaction, ledger_entries ])
+    .from('transactions as t')
+    .join('ledger_entries as le', 't.id', 'le.transaction_id')
+    .groupBy(...transaction)
 }

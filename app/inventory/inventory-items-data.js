a7aa838241f50f796e0ef7b05c5a71d36274927d
thebearingedge <contact@timdav.is>
2016-12-24 17:44:43 -0800

    Moves inventory_items_view to data access.

--- app/inventory/inventory-items-data.js
+++ app/inventory/inventory-items-data.js
@@ -30,10 +30,40 @@ export default function inventoryItemsData(knex) {
   }
 
   function findBySku(sku, trx) {
-    return trx
-      .select('*')
-      .from('inventory_items_view')
-      .where({ sku })
+    return inventoryItemsView(trx)
+      .where('i.sku', sku)
       .first()
   }
+}
+
+function inventoryItemsView(knex) {
+  const quantityOnPurchaseOrder = knex
+    .select(knex.raw('coalesce(sum(l.quantity), 0)::integer'))
+    .from('order_line_items as l')
+    .whereRaw('l.sku = ii.sku and l.order_type = \'purchase_order\'')
+    .as('quantity_on_purchase_order')
+  const quantityOnHand = knex
+    .select(knex.raw('coalesce(sum(s.quantity), 0)::integer'))
+    .from('stock_moves as s')
+    .whereRaw('s.sku = ii.sku')
+    .as('quantity_on_hand')
+  const columns = [
+    'i.sku',
+    'i.description',
+    'i.is_active',
+    'i.created_at',
+    'i.updated_at',
+    'ii.revenue_code',
+    'ii.cost_code',
+    'ii.asset_code',
+    'b.name as brand_name',
+    quantityOnPurchaseOrder,
+    quantityOnHand
+  ]
+  return knex
+    .select(columns)
+    .from('inventory_items as ii')
+    .join('items as i', 'ii.sku', 'i.sku')
+    .leftJoin('brands as b', 'ii.brand_id', 'b.id')
+    .leftJoin('order_line_items as l', 'i.sku', 'l.sku')
 }

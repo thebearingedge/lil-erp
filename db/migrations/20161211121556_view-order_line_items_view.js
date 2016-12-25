f7dd8128a3619a0f20d3f55ac2bf0ca3c5683e1d
thebearingedge <contact@timdav.is>
2016-12-24 17:14:40 -0800

    Fixes table alias in order_line_items_view.

--- db/migrations/20161211121556_view-order_line_items_view.js
+++ db/migrations/20161211121556_view-order_line_items_view.js
@@ -1,9 +1,9 @@
 export const up = knex => {
   const unit_price = knex.raw('(o.line_total / o.quantity)::float as unit_price')
   const quantity_received = knex
-    .select(knex.raw('o.quantity - coalesce(sum(r.quantity), 0)::integer'))
-    .from('shipment_line_items as r')
-    .whereRaw('o.id = r.order_line_item_id')
+    .select(knex.raw('o.quantity - coalesce(sum(s.quantity), 0)::integer'))
+    .from('shipment_line_items as s')
+    .whereRaw('o.id = s.order_line_item_id')
     .as('quantity_remaining')
   const columns = [
     'o.id',
@@ -19,7 +19,7 @@ export const up = knex => {
   const view = knex
     .select(columns)
     .from('order_line_items as o')
-    .leftJoin('shipment_line_items as r', 'o.id', 'r.order_line_item_id')
+    .leftJoin('shipment_line_items as s', 'o.id', 's.order_line_item_id')
   return knex.raw(`create view "order_line_items_view" as ${view}`)
 }
 

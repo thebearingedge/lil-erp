1e6af32ea0193e40662a259deebcf973ddb324db
thebearingedge <contact@timdav.is>
2016-12-25 15:05:40 -0800

    Adds average cost to stock_moves.

--- db/migrations/sql/create_stock_move_shipment_line_item.sql
+++ db/migrations/sql/create_stock_move_shipment_line_item.sql
@@ -1,10 +1,12 @@
 create function create_stock_move_shipment_line_item() returns trigger as $$
   declare
-    sign integer;
-    average_cost float;
-    shipment_date timestamp;
     shipment_line_item_id uuid;
+    shipment_date timestamp;
+    sign integer;
+    previous_quantity_on_hand integer;
+    previous_average_cost float;
     new_quantity_on_hand integer;
+    new_average_cost float;
   begin
 
     shipment_line_item_id := new.id;
@@ -21,30 +23,51 @@ create function create_stock_move_shipment_line_item() returns trigger as $$
       end
     into sign;
 
-    with previous_quantity as (
-      select sm.quantity_on_hand
+    with previous_stock_move as (
+      select sm.quantity_on_hand, sm.average_cost
       from stock_moves as sm
       where sm.sku = new.sku
-      order by sm.shipment_date desc
+      order by
+        sm.shipment_date desc,
+        sm.created_at desc
       limit 1
     )
-    select coalesce(sum(quantity_on_hand), 0) + sign * new.quantity
-    into new_quantity_on_hand
-    from previous_quantity;
+    select quantity_on_hand, average_cost
+    into previous_quantity_on_hand, previous_average_cost
+    from previous_stock_move;
+
+    select
+      new.quantity * sign
+      +
+      coalesce(previous_quantity_on_hand, 0)
+    into new_quantity_on_hand;
+
+    select
+      case when previous_average_cost is not null and sign < 0 then previous_average_cost
+           when previous_average_cost is null and sign > 0 then new.line_total
+           else ((previous_quantity_on_hand * previous_average_cost)
+                +
+                (new.line_total))
+                /
+                (new.quantity + previous_quantity_on_hand)
+      end
+    into new_average_cost;
 
     insert into stock_moves (
       shipment_date,
       shipment_line_item_id,
       sku,
       quantity,
-      quantity_on_hand
+      quantity_on_hand,
+      average_cost
     )
     values (
       shipment_date,
       shipment_line_item_id,
       new.sku,
       new.quantity * sign,
-      new_quantity_on_hand
+      new_quantity_on_hand,
+      new_average_cost
     );
 
     return new;

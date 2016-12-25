e55a729662761b72d2a9a7ad109b27fa308da358
thebearingedge <contact@timdav.is>
2016-12-25 15:08:30 -0800

    Changed database name and user name.

--- db/knexfile.js
+++ db/knexfile.js
@@ -3,8 +3,8 @@ import { join } from 'path'
 export const development = {
   client: 'postgresql',
   connection: {
-    database: 'mini-erp',
-    user: 'mini-erp'
+    database: 'lil-erp',
+    user: 'lil-erp'
   },
   seeds: {
     directory: join(__dirname, 'seeds/development')

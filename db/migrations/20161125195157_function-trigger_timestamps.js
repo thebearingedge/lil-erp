e178e8b259e61298848250ad262b81e1ff125905
thebearingedge <contact@timdav.is>
2016-12-27 15:09:14 -0800

    Preserve "created_at" timestamp on records.

--- db/migrations/20161125195157_function-trigger_timestamps.js
+++ db/migrations/20161125195157_function-trigger_timestamps.js
@@ -2,6 +2,7 @@ import { readSql } from './helpers'
 
 export const up = async ({ raw }) => {
   await raw(await readSql(__dirname, 'set_created_at.sql'))
+  await raw(await readSql(__dirname, 'keep_created_at.sql'))
   await raw(await readSql(__dirname, 'set_updated_at.sql'))
   await raw(await readSql(__dirname, 'trigger_timestamps.sql'))
 }
@@ -9,5 +10,6 @@ export const up = async ({ raw }) => {
 export const down = async ({ raw }) => {
   await raw('drop function trigger_timestamps(table_name regclass)')
   await raw('drop function set_updated_at()')
+  await raw('drop function keep_created_at()')
   await raw('drop function set_created_at()')
 }
--- db/migrations/sql/trigger_timestamps.sql
+++ db/migrations/sql/trigger_timestamps.sql
@@ -6,17 +6,32 @@ create function trigger_timestamps(table_name regclass) returns void as $$
         add column "updated_at" timestamptz
     ', table_name);
     execute format('
-      create trigger set_created_at
+      create trigger set_created_at_insert
         before insert
         on %I
         for each row
         execute procedure set_created_at()
       ', table_name);
     execute format('
-      create trigger set_updated_at
-        before insert or update
+      create trigger set_updated_at_insert
+        before insert
         on %I
         for each row
+        execute procedure set_updated_at()
+    ', table_name);
+    execute format('
+      create trigger keep_created_at_update
+        before update
+        on %I
+        for each row
+        execute procedure keep_created_at()
+    ', table_name);
+    execute format('
+      create trigger set_updated_at_update
+        before update
+        on %I
+        for each row
+        when (new.* is distinct from old.*)
         execute procedure set_updated_at()
       ', table_name);
   end;
